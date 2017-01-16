c349738d5a474780e7d5db945f194709548b27e6
Alexandru Laurus <alexandru.laurus@gmail.com>
2017-01-16 03:19:03 +0200

    Update endpoint with keyword parameter

--- server/app.py
+++ server/app.py
@@ -1,16 +1,15 @@
 from flask import Flask
 from flask import request
 from flask import make_response
-from pymongo import MongoClient
-from datetime import datetime
-import json
+from bson.json_util import dumps
+import pymongo
 import os
 
 app = Flask(__name__)
 host = os.getenv('MONGO_HOST', 'localhost')
 port = os.getenv('MONGO_PORT', '27017')
 
-mongo = MongoClient('mongodb://{}:{}/'.format(host, port))
+mongo = pymongo.MongoClient('mongodb://{}:{}/'.format(host, port))
 db = mongo.reddyt_db
 
 @app.route("/")
@@ -20,22 +19,43 @@ def main():
 @app.route("/items")
 def search():
     try:
+        search_options = {}
+        if request.args.get('keyword') is not None:
+            app.logger.debug("Keyword %s was specified. Will perform text search", 
+                    request.args.get('keyword'))
+            search_options = { '$text' : { '$search' : request.args.get('keyword') } } 
+        
+        search_options['created'] = {}
+        search_options['created'] = {
+            '$gte' : int(request.args.get('from')),
+            '$lt' : int(request.args.get('to'))
+        }
+
+        """
+        Maybe I can validate subrreddit name although an inexistent
+        collection will return 0 results which is actually as expected
+        """
         items = []
-        #TODO: maybe validate subrreddit name although an inexistent
-        #TODO: collection will return 0 results
-        items += db[request.args.get('subreddit')].find( {
-            'created'   : { '$gte' : int(request.args.get('from')) },
-            'created'   : { '$lt' : int(request.args.get('to')) }
-        }, { '_id': 0 })
-        app.logger.debug("Found %d items", len(items))
-        response = make_response(json.dumps(items), 200)
+        items += db[request.args.get('subreddit')].find(
+            search_options, 
+            { '_id': 0 }
+        ).sort([('created', pymongo.DESCENDING)])
+        
+        """
+        'created' gets printed with .0 from json.dumps(). It's also in the same
+        way in the request. However, it's stored correctly as an int in mongo
+        I'm going to make an explicit cast to int()
+        """
+        formatted_items = [{'id':x['id'],x['type']:x['content'],'created':int(x['created'])} for x in items]
+
+        app.logger.debug("Found %d items", len(formatted_items))
+        response = make_response(dumps(formatted_items, indent=4, sort_keys=True), 200)
     except ValueError:
         app.logger.error("Invalid parameters")
-        response = make_response(json.dumps({'error':'invalid request params'}), 400)
+        response = make_response(dumps({'error':'invalid request params'}), 400)
     except TypeError:
         app.logger.error("Missing parameters")
-        response = make_response(json.dumps({'error':'missing request params'}), 400)
-
+        response = make_response(dumps({'error':'missing request params'}), 400)
     response.headers['Content-Type'] = 'application/json'
     return response
 
